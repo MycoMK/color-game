d7abd23eda6ff1eb27e20f731384c776a8964b37
Myco <kmichael831@gmail.com>
2023-02-02 22:22:25 -0800

    edits

--- color.js
+++ color.js
@@ -8,35 +8,36 @@ let colors = [
     "rgb(0, 0, 0)",
     "rgb(255, 255, 0)",
     "rgb(255, 255, 255)",
-]
+// ]
 
-let squares = document.querySelectorAll(".squares");
+// let squares = document.querySelectorAll(".squares");
 
-let pickedColor=colors[3];
-let colorDisplay=document.getElementById("colorDisplay");
-let messageDisplay=document.querySelector("#message")
+// let pickedColor=colors[3];
+// let colorDisplay=document.getElementById("colorDisplay");
+// let messageDisplay=document.querySelector("#message")
 
-colorDisplay.textContent=pickedColor;
+// colorDisplay.textContent=pickedColor;
 
-for(let i = 0; i < squares . length; i++){
- squares[i].style.backgroundColor = colors[i];
+// for(let i = 0; i < squares . length; i++){
+//  squares[i].style.backgroundColor = colors[i];
 
- // add click listener to squares
- squares[i].addEventListener("click", function(){
+//  // add click listener to squares
+//  squares[i].addEventListener("click", function(){
     
-    // grad color of clicked square
-    let clickedColor = this.style.backgroundColor;
-    // compair color of pickedColor
-    if(clickedColor === pickedColor){
-        messageDisplay.textContent= "Correct!";
-        // changeColors("clickedColor");
-    } else{
-        this.style.backgroundColor = "#232323";
-        messageDisplay.textContent="Try Again"
-    }
-});
+//     // grad color of clicked square
+//     let clickedColor = this.style.backgroundColor;
+//     // compair color of pickedColor
+//     if(clickedColor === pickedColor){
+//         messageDisplay.textContent= "Correct!";
+//         // changeColors("clickedColor");
+//     } else{
+//         this.style.backgroundColor = "#232323";
+//         messageDisplay.textContent="Try Again"
+//     }
+// });
 
-};
+// };
+
 
 // function changeColors(colors){
 //     // loop through all squares
